d847f33c66a38c831977e06f7ec8da6e6ab54a61
luisjmc <lucho_mxt@hotamil.com>
2021-12-17 18:58:05 -0300

    DomYjson

--- HTML/js/index.js
+++ HTML/js/index.js
@@ -6,14 +6,13 @@ function saludar(){
 }
 
 //D.O.M.
-
 function MostrarTexto(){                
     const elemento = document.getElementById("intro"); //guardo todo el elemento del id Intro
     const texto = document.getElementById("txt");
     console.log(elemento.value);//solo muestro el valor
 
     elemento.innerHTML= "Paragraph changed!";
-    console.log(elemento);                
+    console.log(elemento);
     console.log(texto.innerHTML);
 
     var tag = document.createElement("p");
@@ -22,4 +21,29 @@ function MostrarTexto(){
     var element = document.getElementById("new"); //traigo el elemento por id
     console.log(element);
     element.appendChild(tag);// agrego el nodo al elemento traido
+
+    var cadenaJson = JSON.stringify(libros);
+    console.log(JSON.stringify(JSONObj));
+    console.log(JSON.stringify(libros));
+
+    var tag = document.createElement("p");
+    var text = document.createTextNode(cadenaJson);
+    tag.appendChild(text);
+    var agregandoJson = document.getElementById("textoNuevo");
+    agregandoJson.appendChild(tag);
 }
+
+var JSONObj = { "nombrelibro":"El hacedor", "precio":500 };	
+var libros =new Object();
+    libros = {
+            "Libros": [
+                {
+                    "nombrelibro": "El hacedor",
+                    "precio": 500
+                },
+                {
+                    "nombrelibro": "El libro gordo de petete",
+                    "precio": 600
+                }
+            ]
+        }
