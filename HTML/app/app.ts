4e3dffa4b1558538a57ef5e96e7ecc1725ee6dff
luisjmc <lucho_mxt@hotmail.com>
2021-12-20 18:16:37 -0300

    typescript

---
+++ HTML/app/app.ts
@@ -0,0 +1,8 @@
+let mensaje:string=", digo yo";
+let message:string= 'Hello World ${mensaje}';
+console.log(message);
+//en tsconfig descomente la entrada outfile para que los js compilados vayan a una carpeta
+//y comente la entrada 'module'
+
+
+//let desc:any[]=[1,true,"verde"];
---
+++ HTML/output/app.js
@@ -0,0 +1,7 @@
+"use strict";
+let mensaje = ", digo yo";
+let message = 'Hello World ${mensaje}';
+console.log(message);
+//en tsconfig descomente la entrada outfile para que los js compilados vayan a una carpeta
+//y comente la entrada 'module'
+//let desc:any[]=[1,true,"verde"];
