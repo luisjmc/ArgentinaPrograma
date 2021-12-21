41e9bd20c23c247630501bb343b93eda6433fc5a
luisjmc <lucho_mxt@hotamil.com>
2021-12-21 03:07:33 -0300

    typescript

--- HTML/app/app.ts
+++ HTML/app/app.ts
@@ -1,8 +1,24 @@
 let mensaje:string=", digo yo";
-let message:string= 'Hello World ${mensaje}';
+let message:string= `Hello World ${mensaje}`;
 console.log(message);
 //en tsconfig descomente la entrada outfile para que los js compilados vayan a una carpeta
 //y comente la entrada 'module'
 
 
 //let desc:any[]=[1,true,"verde"];
+function mensaje2():void{
+    console.log(message);    
+}
+
+//**Crear la enumeración */
+enum Color {
+    Blanco,
+    Rojo,
+    Verde
+}
+ 
+//**Declarar la variable y asignar un valor de la enumeración */
+let colorAuto: Color= Color.Blanco; 
+console.log(colorAuto);
+
+let list:string[]=['pimiento','papas','tomate'];
